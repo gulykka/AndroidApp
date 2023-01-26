73e5cc7f78967c1e805b03d90ec92ac77571a1b6
gulykka <103120773+gulykka@users.noreply.github.com>
2023-01-27 02:38:48 +1000

    add gitignore

--- app/src/main/java/com/example/mytasks/MainActivity.kt
+++ app/src/main/java/com/example/mytasks/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.mytasks
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.general_page)
+        Log.e("AAA", "start")
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.e("AAA","create")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("AAA", "dead")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.e("AAA", "dead444")
     }
 }
